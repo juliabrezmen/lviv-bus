1de69787f6956d9eacadb4a512134812dbdc4fff
Julia Danylyk <julia.danylyk@gmail.com>
2016-02-06 21:56:01 +0200

    Added util for Log.

---
+++ app/src/main/java/com/lvivbus/utils/L.java
@@ -0,0 +1,25 @@
+package com.lvivbus.utils;
+
+import android.util.Log;
+
+public class L {
+
+    static String TAG = "LvivBus";
+
+    public static void i(String msg) {
+        Log.i(TAG, msg);
+    }
+
+    public static void v(String msg) {
+        Log.v(TAG, msg);
+    }
+
+    public static void v(String message, Object... args) {
+        Log.v(TAG, String.format(message, args));
+    }
+
+    public static void w(String msg, Throwable t) {
+        Log.w(TAG, msg, t);
+    }
+
+}
